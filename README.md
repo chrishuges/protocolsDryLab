f4436b1f3e638abb8b156bee2248eae93d1ec66a
chrishuges <chrishuges@gmail.com>
2020-06-08 14:12:39 -0700

    update README

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
 # Protocols for dry lab experiments <!-- omit in toc -->
 
-Descriptive protocols for different types of dry-lab processes. The majority of the files are provided as R notebook html files, for ease of visualization. However, Rmd files are provided for all tutorials that can be downloaded and opened on your own machine to follow along if you would like. To download one of the Rmd files, simply navigate to the top level directory for the protocol you are interested in, click on the Rmd file you would like to download, click the 'Raw' button on the file that appears, and right click and hit 'Save As' on the new page that opens. Save the file as an Rmd file and open in RStudio.
-
-In most of these protocols I use R, but some use Python (sometimes in the same notebook). In these instances, I do my best to detail my choice for one vs. the other, and what I am doing.
-
-## Quick links <!-- omit in toc -->
-
-- [Related to Published Data](https://github.com/chrishuges/wetLabProtocols/tree/master/relatedToPublishedData)
-  - [Retrieve array data from GEO and do a survival analysis](http://htmlpreview.github.io/?https://github.com/chrishuges/protocolsDryLab/blob/master/relatedToPublishedData/getGeoDataForSurvivalAnalysis.nb.html)
-- [Related to Proteomics](https://github.com/chrishuges/wetLabProtocols/tree/master/relatedToProteomics)
-- [Related to Next Generation Sequencing](https://github.com/chrishuges/wetLabProtocols/tree/master/relatedToNextGenSequencing)
-- [Troubleshooting](#troubleshooting)
-
-<hr style="height:6pt; visibility:hidden;" />
+Descriptive protocols for different types of dry-lab processes. This page serves as the main repository for the raw and supplementary protocol files. In order to view the html notebook versions, please visit the protocols page on my [GitHub homepage](https://chrishuges.github.io/protocols/).
 
 ## Troubleshooting
 
--- relatedToPublishedData/getGeoDataForSurvivalAnalysisFunctions.R
+++ relatedToPublishedData/getGeoDataForSurvivalAnalysisFunctions.R
@@ -1,57 +1,62 @@
 #######################################################################
 #######################################################################
-##this function performs survival analysis for input genes
-##based on provided expression and phenotype data
+##This function performs survival analysis for input genes.
+##For reference on the format of expression and phenotypeData,
+##please see the example analysis in getGeoDataForSurvivalAnalysis.Rmd
+##that can be found on this GitHub page.
 #######################################################################
 survivalAnalysis = function(geneOfInterest, expressionData, phenotypeData,
                             survivalPlot = FALSE, survivalData = FALSE, writeDirectory = '', ...){
-  ##
+  print(paste('Performing analysis for', geneOfInterest))
+  ########
+  #first we select our gene of interest, and sort based on median expression across all patients
   geneOfInterestExprs = expressionData %>%
     filter(grepl(geneOfInterest, symbol))
   geneOfInterestExprs$medExprs = apply(geneOfInterestExprs[,4:ncol(geneOfInterestExprs)], 1, function(x) median(x, na.rm = TRUE))
   geneOfInterestSort = geneOfInterestExprs %>%
     arrange(desc(medExprs))
-  ##
+  ########
+  #keep the top probe for the gene, based on the expression calculated above
   geneSurvivalInput = geneOfInterestSort[1,1:(ncol(geneOfInterestSort) - 1)] %>%
     pivot_longer(cols = colnames(geneOfInterestSort)[4]:colnames(geneOfInterestSort)[(ncol(geneOfInterestSort) - 1)], 
                  names_to = 'geo_accession', values_to = 'rnaExprs') %>%
     right_join(phenotypeData) %>%
     arrange(desc(rnaExprs)) 
-  ##
+  ########
+  #sorting of the patients into low and high expression based on the top and bottom 25%
   geneSurvivalInput$geneLevel = ''
   geneSurvivalInput[1:round(nrow(geneSurvivalInput) * 0.25), 'geneLevel'] = 'high'
   geneSurvivalInput[round(nrow(geneSurvivalInput) * 0.75):nrow(geneSurvivalInput), 'geneLevel'] = 'low'
   geneSurvivalInput$geneLevel = factor(geneSurvivalInput$geneLevel, levels = c('low','high'))
-  ##
-  
-  ##
+  ########
+  #calculation of the different survival metrics based on our data
   survivalFit = survfit(Surv(ovs, status) ~ geneLevel, data = geneSurvivalInput)
   survivalDiff = survdiff(Surv(ovs, status) ~ geneLevel, data = geneSurvivalInput)
   survivalPValue = 1 - pchisq(survivalDiff$chisq, length(survivalDiff$n) - 1)
+  survivalSummary = surv_summary(survivalFit, data = geneSurvivalInput)
   coxStats = coxph(Surv(ovs, status) ~ geneLevel, data = geneSurvivalInput)
   coxZScore = coef(coxStats)/sqrt(diag(vcov(coxStats)))
-  ##
+  ########
+  #this will output a survival plot
   if (survivalPlot == TRUE){
-    if (nchar(writeDirectory) <= 3){
-      message(paste('No write directory specified. Plots will be written to', baseRepository))
-    }
-    ggsurvplot(survivalFit, pval = survivalPValue, conf.int = FALSE,
-               risk.table = FALSE, linetype = "strata", ggtheme = theme_classic(), palette = brewer.pal(8,'RdBu')[c(8,1)])
+    ggsurvplot(survivalSummary, pval = survivalPValue, conf.int = FALSE,
+               risk.table = FALSE, linetype = "strata", ggtheme = theme_classic(), palette = brewer.pal(8,'RdBu')[c(8,1)]
+               )
     ggsave(paste(baseRepository, writeDirectory, '/survival_', 
                  geneOfInterest, '.pdf', sep = ''), width = 4, height = 4, useDingbats = FALSE)
   }
-  ##
+  ########
+  #this will output a text file with the survival results
   if (survivalData == TRUE){
-    if (nchar(writeDirectory) <= 3){
-      message(paste('No write directory specified. Results will be written to', baseRepository))
-    }
-    survivalOutput = summary(survivalFit)$table
-    head(survivalOutput)
-    write.table(survivalOutput, paste(baseRepository, writeDirectory, '/survival_', geneOfInterest, '.csv', sep = ''),
-                col.names = TRUE, row.names = TRUE, quote = FALSE, sep = ',')
+  survivalOutput = summary(survivalFit)$table
+  write.table(survivalOutput, paste(baseRepository, writeDirectory, '/survival_', geneOfInterest, '.csv', sep = ''),
+              col.names = TRUE, row.names = TRUE, quote = FALSE, sep = ',')
   }
-  ##
+  ########
+  #lastly we output the Z-score
   return(coxZScore)
+  
 }
 #######################################################################
 #######################################################################
+
